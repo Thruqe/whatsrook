b86a18590755c160d153c3ca99ceb019a5b183b7
Thruqe <danielpeter0039@gmail.com>
2026-07-10 19:42:15 +0100

    add a few more commands

--- commands/helper.go
+++ commands/helper.go
@@ -1,10 +1,58 @@
 package commands
 
-import "go.mau.fi/whatsmeow/proto/waE2E"
+import (
+	"net/url"
+	"strings"
+
+	"go.mau.fi/whatsmeow/proto/waE2E"
+)
 
 func sendText(ctx *Context, text string) error {
 	_, err := ctx.Client.SendMessage(ctx.Ctx, ctx.Chat, &waE2E.Message{
 		Conversation: new(text),
 	})
 	return err
+}
+
+func isFacebookURL(link string) bool {
+	return matchesHost(link, "facebook.com", "fb.com", "fb.watch")
+}
+
+func isInstagramURL(link string) bool {
+	return matchesHost(link, "instagram.com")
+}
+
+func isTwitterURL(link string) bool {
+	return matchesHost(link, "twitter.com", "x.com")
+}
+
+func isThreadsURL(link string) bool {
+	return matchesHost(link, "threads.net")
+}
+
+func isYouTubeURL(link string) bool {
+	return matchesHost(link, "youtube.com", "youtu.be")
+}
+
+func isTikTokURL(link string) bool {
+	return matchesHost(link, "tiktok.com")
+}
+
+// matchesHost parses the URL and checks if its host matches
+// any of the given domains (including subdomains like www.).
+func matchesHost(link string, domains ...string) bool {
+	u, err := url.Parse(strings.TrimSpace(link))
+	if err != nil || u.Host == "" {
+		return false
+	}
+	host := strings.ToLower(u.Host)
+	host = strings.TrimPrefix(host, "www.")
+	host = strings.TrimPrefix(host, "m.")
+
+	for _, d := range domains {
+		if host == d || strings.HasSuffix(host, "."+d) {
+			return true
+		}
+	}
+	return false
 }
--- commands/tiktok.go
+++ commands/tiktok.go
@@ -19,6 +19,9 @@ func handleTikTok(ctx *Context) error {
 	if len(ctx.Args) == 0 {
 		return sendText(ctx, "Usage: !tiktok <url>")
 	}
+	if !isTikTokURL(ctx.Args[0]) {
+		return sendText(ctx, "_Invaild tiktok url!_")
+	}
 	data, err := ember.Fetch(ctx.Ctx, ctx.Args[0])
 	if err != nil {
 		return sendText(ctx, fmt.Sprintf("Failed: %s", err))
