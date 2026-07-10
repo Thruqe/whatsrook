54bfad6b5050b7f85e36698680e67a754216cf74
Thruqe <danielpeter0039@gmail.com>
2026-07-10 19:42:15 +0100

    add uptime command

---
+++ commands/uptime.go
@@ -0,0 +1,21 @@
+package commands
+
+import (
+	"fmt"
+	"time"
+)
+
+var startTime = time.Now()
+
+func init() {
+	Register(&Command{
+		Name:        "uptime",
+		Description: "Show how long the bot has been running",
+		Handler:     handleUptime,
+	})
+}
+
+func handleUptime(ctx *Context) error {
+	d := time.Since(startTime).Round(time.Second)
+	return sendText(ctx, fmt.Sprintf("_Uptime: %s_", d))
+}
