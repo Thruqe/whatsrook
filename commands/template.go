82aa4b4abb92253bebb6ec034dba4cd45a073b1b
Thruqe <danielpeter0039@gmail.com>
2026-07-20 20:53:28 +0100

    Add template message command and unit tests

---
+++ commands/template.go
@@ -0,0 +1,63 @@
+package commands
+
+import (
+	"go.mau.fi/whatsmeow/proto/waE2E"
+	"google.golang.org/protobuf/proto"
+)
+
+func init() {
+	Register(&Command{
+		Name:        "template",
+		Description: "Send an interactive legacy template message",
+		Category:    "example",
+		IsPublic:    true,
+		Handler:     handleTemplate,
+	})
+}
+
+func handleTemplate(ctx *Context) error {
+	msg := &waE2E.Message{
+		DocumentWithCaptionMessage: &waE2E.FutureProofMessage{
+			Message: &waE2E.Message{
+				TemplateMessage: &waE2E.TemplateMessage{
+					HydratedTemplate: &waE2E.TemplateMessage_HydratedFourRowTemplate{
+						HydratedContentText: proto.String("📝 *Template Message Demo*\n\nThis is a legacy template message format containing call-to-action buttons."),
+						HydratedFooterText:  proto.String("Powered by Thruqe"),
+						HydratedButtons: []*waE2E.HydratedTemplateButton{
+							{
+								Index: proto.Uint32(1),
+								HydratedButton: &waE2E.HydratedTemplateButton_QuickReplyButton{
+									QuickReplyButton: &waE2E.HydratedTemplateButton_HydratedQuickReplyButton{
+										DisplayText: proto.String("👋 Quick Reply"),
+										ID:          proto.String("temp_qr"),
+									},
+								},
+							},
+							{
+								Index: proto.Uint32(2),
+								HydratedButton: &waE2E.HydratedTemplateButton_UrlButton{
+									UrlButton: &waE2E.HydratedTemplateButton_HydratedURLButton{
+										DisplayText: proto.String("🌐 Open Link"),
+										URL:         proto.String("https://github.com/Thruqe/whatsrook"),
+									},
+								},
+							},
+							{
+								Index: proto.Uint32(3),
+								HydratedButton: &waE2E.HydratedTemplateButton_CallButton{
+									CallButton: &waE2E.HydratedTemplateButton_HydratedCallButton{
+										DisplayText: proto.String("📞 Call Us"),
+										PhoneNumber: proto.String("+1234567890"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err := ctx.Client.SendMessage(ctx.Ctx, ctx.Chat, msg)
+	return err
+}
